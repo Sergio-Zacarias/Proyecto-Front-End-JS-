b6232596228241de8559745829ddbb53c44e9a46
Sergio Daniel Zacarias <sergiodcarp@gmail.com>
2025-11-21 03:31:07 -0300

    Llegamos hasta ui, cuesta pero vamos

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,10 @@
-export const actualizarContador = (carrito) => {};
+export const actualizarContador = (carrito) => {
+  const contador = document.getElementById("contador-carrito");
+  if (contador) {
+    contador.textContent = carrito.length;
+  }
+};
+
+export const mostrarMensaje = (mensaje) => {
+  alert(mensaje);
+};
