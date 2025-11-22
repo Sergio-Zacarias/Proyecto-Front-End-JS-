2d35bd9f68a2200a6475155834fa43749a073070
Sergio Daniel Zacarias <sergiodcarp@gmail.com>
2025-11-22 01:20:01 -0300

    Comenzando con indexjs

---
+++ js/funcionesCarrito.js
@@ -0,0 +1,26 @@
+import { guardarCarrito, obtenerCarrito, vaciarCarrito } from "./storage.js";
+import { actualizarContador, mostrarMensaje } from "./ui.js";
+
+export const agregarAlCarrito = (producto) => {
+  const carrito = obtenerCarrito();
+  carrito.push(producto);
+
+  guardarCarrito(carrito);
+  actualizarContador(carrito);
+  mostrarMensaje("Producto agregado con exito");
+};
+
+export const eliminarProducto = (indice) => {
+  const carrito = obtenerCarrito();
+  carrito.splice(indice, 1);
+
+  guardarCarrito(carrito);
+  actualizarContador(carrito);
+  mostrarMensaje("Producto eliminado");
+};
+
+export const vaciarCarrito = () => {
+  vaciarCarrito();
+  actualizarContador([]);
+  mostrarMensaje("Carrito vaciado.");
+};
